2c2646d86d26cce59562f78de0e028a41fdce860
Lisitsa-requin <fueicebell@gmail.com>
2021-02-09 01:24:12 +0900

    aboutページの年齢を算出できるように

---
+++ page/about.js
@@ -0,0 +1,24 @@
+const about = new Vue({
+    el: '#about',
+    data:{ 
+        Age: "",
+        birthday: new Date('1998-09-28'),
+    },
+
+    created(){
+        this.calcAge()
+    },
+
+    methods: {
+        calcAge: function(){
+            var now = new Date();
+            var birth = now.getTime()  - this.birthday.getTime();
+            console.log(birth)
+            var ageDate = new Date(birth);
+            console.log(ageDate)
+            this.Age = ageDate.getUTCFullYear() -1970;
+            
+        },
+    },
+    
+});  
