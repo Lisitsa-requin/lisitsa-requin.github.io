d7804e94cd1a01cf8a69459a9a48c02ecc1bee38
Lisitsa-requin <fueicebell@gmail.com>
2021-02-10 00:07:42 +0900

    ストップウォッチ機能を実装(誰得感はあるけど)

--- page/clock.js
+++ page/clock.js
@@ -3,7 +3,14 @@ const clock = new Vue({
     data:{ 
         date: '',
         time: '',
-        week: ['Sun','Mon','Tue','Wen','Thu','Fri','Sat'],
+        week: ['Sun','Mon','Tue','Wen','Thu','Fri','Sat'],  //曜日格納用
+        clockMode: true,    //時計モード(true)orストップウォッチモード(false)
+        //以下ストップウォッチモードの際に使う変数
+        startTime: 0,       //開始時間
+        timer:  0,          //setinterval格納用
+        interval: 0,        //計測時間
+        stopTime: 0,        //累計時間
+        active: false,      //ストップウォッチを実行しているか
     },
 
     mounted(){
@@ -25,7 +32,34 @@ const clock = new Vue({
                 zero += '0';
             }
             return (zero+num).slice(-len);
-        }
+        },
+
+        modeChange: function(){
+            this.clockMode = !this.clockMode;
+            if(this.active){
+                this.timerReset();
+                this.stopTime();
+            }
+        },
+
+        timerStart: function(){
+            this.active = true;
+            this.startTime = Date.now();
+            this.timer = setInterval(()=>{ this.interval = this.stopTime + (Date.now() - this.startTime) * 0.001;},1);
+        },
+
+        timerStop: function(){
+            this.active = false;
+            this.stopTime = this.interval;
+            clearInterval(this.timer);
+        },
+
+        timerReset: function(){
+            this.interval = 0;
+            this.stopTime = 0;
+            this.startTime = Date().now;
+        },
+
     },
     
 });  
